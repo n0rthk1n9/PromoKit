ae9db66438e27d4388268f728e9ecb93a9ac9ff9
Jan Armbrust <jan.armbrust@googlemail.com>
2024-12-05 09:33:17 +0100

    Add view model for promocodes view

---
+++ PromoKit/ViewModels/PromoCodesViewModel.swift
@@ -0,0 +1,36 @@
+//
+//  PromoCodesViewModel.swift
+//  PromoKit
+//
+//  Created by Jan Armbrust on 05.12.2024.
+//
+
+import SwiftUI
+
+@Observable
+class PromoCodesViewModel {
+    var promoCodes: [PromoCode] = []
+
+    func readPromoCodes(from fileURL: URL) {
+        do {
+            // Start accessing the security-scoped resource
+            guard fileURL.startAccessingSecurityScopedResource() else {
+                print("Unable to access the file at \(fileURL.path).")
+                return
+            }
+            defer { fileURL.stopAccessingSecurityScopedResource() }  // Ensure it is released
+
+            // Read the file contents
+            let fileContents = try String(contentsOf: fileURL, encoding: .utf8)
+            let lines = fileContents.split(separator: "\n")
+
+            promoCodes =
+                lines
+                .map { $0.trimmingCharacters(in: .whitespacesAndNewlines) }
+                .filter { !$0.isEmpty }
+                .map { PromoCode(code: $0) }
+        } catch {
+            print("Error reading file: \(error)")
+        }
+    }
+}

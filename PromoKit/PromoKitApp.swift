7e08099b84635c29225acdb275ba53f7e59e5b2b
Jan Armbrust <jan.armbrust@googlemail.com>
2024-12-05 09:13:31 +0100

    Initial commit

---
+++ PromoKit/PromoKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  PromoKitApp.swift
+//  PromoKit
+//
+//  Created by Jan Armbrust on 05.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct PromoKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

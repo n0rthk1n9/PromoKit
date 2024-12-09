cb08a23c4ff2fb1d9e8a38a0d2dd3199d74b2d4b
Jan Armbrust <jan.armbrust@googlemail.com>
2024-12-09 11:21:03 +0100

    Add function to add dummy data

--- PromoKit/Views/PromoAppsView.swift
+++ PromoKit/Views/PromoAppsView.swift
@@ -16,7 +16,7 @@ struct PromoAppsView: View {
     @State private var copyMode: CopyMode = .code
     @State private var appStorePromoCodeLink: String = ""
 
-    @Query private var promoApps: [PromoApp]
+    @Query(sort: \PromoApp.name) private var promoApps: [PromoApp]
 
     enum CopyMode: String, CaseIterable, Identifiable {
         case code = "Code"
@@ -36,37 +36,49 @@ struct PromoAppsView: View {
 
     var body: some View {
         NavigationStack {
-            Picker("Copy Mode", selection: $copyMode) {
-                ForEach(CopyMode.allCases) { mode in
-                    Text(mode.rawValue).tag(mode)
-                }
-            }
-            .pickerStyle(.segmented)
-            .padding(.horizontal)
-            List {
-                ForEach(promoApps) { promoApp in
-                    VStack(alignment: .leading) {
-                        Text(promoApp.name)
-                            .font(.largeTitle)
-                            .bold()
-                            .fontDesign(.rounded)
-                        promoCodesGrid(for: promoApp)
+            VStack {
+                Picker("Copy Mode", selection: $copyMode) {
+                    ForEach(CopyMode.allCases) { mode in
+                        Text(mode.rawValue).tag(mode)
                     }
-                    .padding(.vertical)
                 }
-                .onDelete(perform: deletePromoApp)
-                .listRowBackground(
-                    RoundedRectangle(cornerRadius: 10)
-                        .fill(.secondary.opacity(0.2))
-                        .overlay(
+                .pickerStyle(.segmented)
+                .padding(.horizontal)
+                if !promoApps.isEmpty {
+                    List {
+                        ForEach(promoApps) { promoApp in
+                            VStack(alignment: .leading) {
+                                Text(promoApp.name)
+                                    .font(.largeTitle)
+                                    .bold()
+                                    .fontDesign(.rounded)
+                                promoCodesGrid(for: promoApp)
+                            }
+                            .padding(.vertical)
+                        }
+                        .onDelete(perform: deletePromoApp)
+                        .listRowBackground(
                             RoundedRectangle(cornerRadius: 10)
-                                .stroke(.secondary, lineWidth: 1)
+                                .fill(.secondary.opacity(0.2))
+                                .overlay(
+                                    RoundedRectangle(cornerRadius: 10)
+                                        .stroke(.secondary, lineWidth: 1)
 
+                                )
+                                .padding(.vertical, 10)
+                                .padding(.horizontal, 1)
                         )
-                        .padding(.vertical, 10)
-                        .padding(.horizontal, 1)
-                )
-                .listRowSeparator(.hidden)
+                        .listRowSeparator(.hidden)
+                    }
+                } else {
+                    ContentUnavailableView {
+                        Button {
+                            generateSampleData()
+                        } label: {
+                            Text("Add demo app")
+                        }
+                    }
+                }
             }
             .navigationTitle("Promo Kit")
             .toolbar {
@@ -96,7 +108,6 @@ struct PromoAppsView: View {
             .sheet(isPresented: $showAddPromoAppSheet) {
                 AddPromoAppView()
             }
-
         }
         .overlay {
             if copiedToClipboard {
@@ -149,6 +160,41 @@ struct PromoAppsView: View {
                 context.delete(promoAppToDelete)
             }
         }
+    }
+
+    // TODO: Remove bevor deplayment
+    func generateSampleData() {
+        // Create new instances of promo apps
+        let promoApp1 = PromoApp(name: "App Exhibit", version: "1.3", appId: "6503256642")
+        let promoApp2 = PromoApp(name: "Cosmo Pic", version: "1.0", appId: "129381232")
+
+        // Insert promo apps into the context
+        context.insert(promoApp1)
+        context.insert(promoApp2)
+
+        // Create promo codes for promoApp1
+        let promoCode1 = PromoCode(code: "AF4HYL4EETAP", dateAdded: .now, used: false)
+        let promoCode2 = PromoCode(code: "NTYEJLYFHYJT", dateAdded: .now, used: true)
+        let promoCode3 = PromoCode(code: "7HMHWLA6A9JM", dateAdded: .now, used: false)
+
+        // Assign the promo codes to promoApp1
+        promoCode1.promoApp = promoApp1
+        promoCode2.promoApp = promoApp1
+        promoCode3.promoApp = promoApp1
+
+        // Insert promo codes into the context and add them to promoApp1's promoCodes array
+        context.insert(promoCode1)
+        context.insert(promoCode2)
+        context.insert(promoCode3)
+        promoApp1.promoCodes.append(contentsOf: [promoCode1, promoCode2, promoCode3])
+
+        // Create a promo code for promoApp2
+        let promoCode4 = PromoCode(code: "AF4HYL4EETAP", dateAdded: .now, used: false)
+        promoCode4.promoApp = promoApp2
+
+        // Insert promo code into the context and add it to promoApp2's promoCodes array
+        context.insert(promoCode4)
+        promoApp2.promoCodes.append(promoCode4)
     }
 }
 
